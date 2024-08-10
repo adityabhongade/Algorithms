1d62330cbbfee486607bad43e12dec564ff8535f
Aditya Bhongade <129876695+adityabhongade@users.noreply.github.com>
2024-08-10 12:31:26 +0530

    Create template1.cpp

---
+++ Templates/template1.cpp
@@ -0,0 +1,50 @@
+/*
+
+template1.cpp from 
+by Aditya Bhongade - https://github.com/adityabhongade
+
+Description : Basic CP template for beginners.
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define sz(x) x.size()
+#define nl "\n"
+
+const int ARR_MAX = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = INT_MAX;
+const db PI = 3.1415926;
+
+void run_test() {
+    // code here
+    return;
+}        
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t=1;
+    cin>>t;
+    while(t--) {
+        run_test();
+        cout<<nl;
+    }
+    return 0;
+}
